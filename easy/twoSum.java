632996555956e2ad95fb18d01cfc2312bec09d39
RahulPranami <rahulpranami101@gmail.com>
2022-07-03 15:06:09 +0530

    adding twoSum functions

---
+++ easy/twoSum.java
@@ -0,0 +1,20 @@
+class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length-1;i++)
+            for(int j=i+1; j<nums.length; j++)
+            {
+                if(nums[i]+nums[j]==target)
+                    return new int[]{i,j};
+            }
+        return new int[]{-1,-1};
+        
+    }
+        
+        
+    public void main(String args[]){
+        int[] nums={2,7,11,15};
+        int target=17;
+        for(int x: twoSum(nums , target))
+            System.out.print(x);
+    }
+}
